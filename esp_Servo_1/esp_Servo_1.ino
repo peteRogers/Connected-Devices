01b3fe1d27c9a9240894b8f056d9ff2ff73bc631
Peter Rogers <p.rogers@gold.ac.uk>
2019-10-23 22:14:48 +0100

    Add files via upload

---
+++ esp_Servo_1/esp_Servo_1.ino
@@ -0,0 +1,27 @@
+
+// Include the ESP32 Arduino Servo Library instead of the original Arduino Servo Library
+#include <ESP32Servo.h> 
+
+Servo myservo; 
+
+
+int servoPin = 12;      
+
+int potPin = 34;       
+int ADC_Max = 4096;    
+                       
+  
+int val;    
+void setup(){ 
+  Serial.begin(115200);
+  myservo.setPeriodHertz(50);
+  myservo.attach(servoPin, 500, 2400);   
+}
+
+void loop() {
+  val = analogRead(potPin);          
+  val = map(val, 0, ADC_Max, 10, 155); 
+  myservo.write(val);
+  Serial.println(val);
+  delay(20);                          
+}
