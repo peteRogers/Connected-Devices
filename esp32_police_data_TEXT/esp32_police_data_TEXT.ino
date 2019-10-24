3cb822b50a516d2c9aa9198d047a88bc321ea42f
Peter Rogers <p.rogers@gold.ac.uk>
2019-10-25 00:59:56 +0100

    change pass

--- esp32_police_data_TEXT/esp32_police_data_TEXT.ino
+++ esp32_police_data_TEXT/esp32_police_data_TEXT.ino
@@ -20,11 +20,10 @@ int pass = 0;
 CRGB matrixleds[NUMMATRIX];
 
 FastLED_NeoMatrix *matrix = new FastLED_NeoMatrix(matrixleds, mw, mh, mw/8, 1, NEO_MATRIX_TOP + NEO_MATRIX_RIGHT + NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE );
-const char* ssid = "VM981001-2G";
-const char* password =  "susieAmy";
 
-//const char* ssid = "labLAN";
-//const char* password =  "password";
+
+const char* ssid = "labLAN";
+const char* password =  "password";
 String cString = "";
 
 const String endpoint = "http://api.openweathermap.org/data/2.5/forecast?id=6947041&cnt=5&units=metric&APPID=";
