11eb43ee53acc4948709cf882aca1c2da4fc9733
Peter Rogers <p.rogers@gold.ac.uk>
2019-10-24 23:13:59 +0100

    updating for weather forecast

--- esp32_weather_data_TEXT/esp32_weather_data_TEXT.ino
+++ esp32_weather_data_TEXT/esp32_weather_data_TEXT.ino
@@ -1,14 +1,13 @@
 #include <ArduinoJson.h>
-
 #include <WiFi.h>
 #include <HTTPClient.h>
- #include <Adafruit_GFX.h>
+#include <Adafruit_GFX.h>
 #include <FastLED.h>
 #include <FastLED_NeoMatrix.h>
 
 #define PIN 21
 
-long del = 0;
+long del = -60000;
 
 #define mw 8 //amount of pixels width
 #define mh 8 //amount of pixels height
@@ -20,83 +19,59 @@ int pass = 0;
 
 CRGB matrixleds[NUMMATRIX];
 
-FastLED_NeoMatrix *matrix = new FastLED_NeoMatrix(matrixleds, mw, mh, mw/8, 1, 
-  NEO_MATRIX_TOP     + NEO_MATRIX_RIGHT +
-    NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE );
+FastLED_NeoMatrix *matrix = new FastLED_NeoMatrix(matrixleds, mw, mh, mw/8, 1, NEO_MATRIX_TOP + NEO_MATRIX_RIGHT + NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE );
 
-const char* ssid = "LabLAN";
+const char* ssid = "labLAN";
 const char* password =  "password";
- String cString = "";
-const String endpoint = "http://api.openweathermap.org/data/2.5/weather?id=6947041&APPID=";
+String cString = "";
+
+const String endpoint = "http://api.openweathermap.org/data/2.5/forecast?id=6947041&cnt=5&units=metric&APPID=";
 const String key = "ebf1fe041a7eabcc11f8e1bc1641d10d";
  
 void setup() {
  
   Serial.begin(115200);
- 
   WiFi.begin(ssid, password);
- 
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
- 
   Serial.println("Connected to the WiFi network");
-
- FastLED.addLeds<NEOPIXEL,PIN>(matrixleds, NUMMATRIX); 
+  FastLED.addLeds<NEOPIXEL,PIN>(matrixleds, NUMMATRIX); 
   matrix->begin();
   matrix->setTextWrap(false);
   matrix->setBrightness(255);
-  //matrix->setTextColor(colors[0]);
 }
  
 void loop() {
- 
-
-  if( millis() > del + 30000){
-    Serial.println("foof");
+  if(millis() > del + 60000){
     del = millis();
-      if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
- 
-    HTTPClient http;
- 
-    http.begin(endpoint + key); //Specify the URL
-  // http.begin("http://api.erg.kcl.ac.uk/AirQuality/Daily/MonitoringIndex/Latest/SiteCode=LW1/Json");
-    int httpCode = http.GET();  //Make the request
- 
-    if (httpCode > 0) { //Check for the returning code
- 
-        String payload = http.getString();
-       // Serial.println(httpCode);
-        Serial.println(payload);
-       
-
-        DynamicJsonDocument doc(1024);
-        
-        deserializeJson(doc, payload);
-//        
-//        JsonObject obj = doc.as<JsonObject>();
-//        JsonVariant object = obj.getMember("DailyAirQualityIndex");
-//        JsonVariant la = object.getMember("LocalAuthority");
-//        JsonVariant site = la.getMember("Site");
-//        JsonVariant spec = site.getMember("Species");
-       // int d = doc["DailyAirQualityIndex"]["LocalAuthority"]["Site"]["Species"]["@AirQualityIndex"];
-       String d = doc["rain"]["1h"];
-  
-    //delay(5);
-        
-        //int d = object[String("@AirQualityIndex")];
-       // Serial.println(d);
-        cString = d;
-
-        //////
-      }
- 
-    else {
-     // Serial.println("Error on HTTP request");
-    }
- 
-    http.end(); //Free the resources
+    if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
+       HTTPClient http;
+       http.begin(endpoint+key); //Specify the URL
+       int httpCode = http.GET();  //Make the request
+     
+        if (httpCode > 0) { //Check for the returning code
+            String payload = http.getString();
+            Serial.println(payload);
+             
+            DynamicJsonDocument doc(4096);
+            deserializeJson(doc, payload);
+            
+            for(int i = 0; i < doc["list"].size(); i ++){
+              String date = doc["list"][i]["dt_txt"];
+              String d = doc["list"][i]["weather"][0]["description"];
+              Serial.println(date);
+              Serial.println(d);
+              
+            }
+          }
+     
+        else {
+         // Serial.println("Error on HTTP request");
+        }
+     
+        http.end(); //Free the resources
   }
  
   }
